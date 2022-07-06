c25f6152a67d8af5e0e24b25f994714b3201ddb9
Andre Cruz <mariliaeandre.iba@hotmail.com>
2022-07-06 16:14:24 -0300

    Jogo da memória - Super Mario World

---
+++ scripts.js
@@ -0,0 +1,60 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+/*Adiciona ou retira a classe flip de cada card quando ele é clicado. */
+function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+/**Vefifica a igualdade das cartas */
+function checkForMath(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards(){
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 800)
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
+
+cards.forEach((card) => {
+    card.addEventListener("click", flipCard)
+})
